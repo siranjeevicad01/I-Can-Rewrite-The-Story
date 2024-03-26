d54a4fbb9179bc993effc38207d123670031d144
Siranjeevi S <siranjeevicad01@brightskills.com>
2024-03-26 18:02:08 +0530

    edited a login controller

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,15 +1,16 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using I_Can_Rewrite_The_Story.Models;
+using Microsoft.Data.SqlClient;
 
 namespace I_Can_Rewrite_The_Story.Controllers;
 
+SqlConnection con=new SqlConnection();
+    SqlCommand com=new SqlCommand();
+    SqlDataReader? dr;
+
 public class LoginController : Controller
 {
-    SqlConnection con = new SqlConnection();
-    SqlCommand cmd = new SqlCommand();
-    SqlDataReader dr;
-
     private readonly ILogger<LoginController> _logger;
 
     public LoginController(ILogger<LoginController> logger)
@@ -47,18 +48,33 @@ public class LoginController : Controller
     {
         return View();
     }
-    void ConnectionSring(){
-        con.ConnectionSring="data source=192.168.1.240\SQLEXPRESS;database=RMS; User Id=CADBATCH01;Password=CAD@123pass; TrustServerCertificate=True;"
+    
+    void ConnectionString(){
+    con.ConnectionString = "Data Source=192.168.1.240\\SQLEXPRESS;Initial Catalog=RMS;User ID=CADBATCH01;Password=CAD@123pass;TrustServerCertificate=True;";
     }
     
     [HttpPost]
     public IActionResult RegisterDB(RegisterModel rmodel)
     {
-        ConnectionSring();
+        ConnectionString();
         con.Open();
-        cmd.Connection=con;
-        cmd.CommandText
-        return View();
+        com.Connection=con;
+        com.CommandText="insert into Usr_Reg(FirstName,LastName,User_name,Email_Id,Password) values(@FirstName,@LastName,@User_name,@Email_Id,@Password)";
+        com.Parameters.AddwithValue("@FirstName",rmodel.FirstName);
+        com.Parameters.AddwithValue("@LastName",rmodel.LastName);
+        com.Parameters.AddwithValue("@User_name",rmodel.User_name);
+        com.Parameters.AddwithValue("@Email_Id",rmodel.Email_Id);
+        com.Parameters.AddwithValue("@Password",rmodel.Password);
+        int rowAffected=cmd.ExecuteNonquery();
+        if(rowAffected>0){
+            con.Close();
+            return RedirectToAction("Login");
+        }
+        else
+        {
+            con.Close();
+            return view("Error");
+        }
     }
 
 
---
+++ Models/RegisterModel.cs
@@ -0,0 +1,12 @@
+namespace I_Can_Rewrite_The_Story.Models;
+
+public class RegisterModel
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? User_name { get; set; }
+    public string? Email_Id { get; set; }
+    public string? Password { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
