dd4c140a74b8abaca45f06730159c99fca727743
Siranjeevi S <siranjeevicad01@brightskills.com>
2024-04-01 18:59:02 +0530

    added a dotnet watch package

--- Models/RegisterModel.cs
+++ Models/RegisterModel.cs
@@ -1,10 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace I_Can_Rewrite_The_Story.Models;
 
 public class RegisterModel
 {
-    public string? Reg_ID { get; set; }
-    public string? User_name { get; set; }
-    public string? Email_Id { get; set; }
-    public string? Create_Password { get; set; }
-    public string? Repeat_Password { get; set; }
+    public string User_name { get; set; }
+    public string Email_Id { get; set; }
+    public string Create_Password { get; set; }
+    public string Repeat_Password { get; set; }
 }
