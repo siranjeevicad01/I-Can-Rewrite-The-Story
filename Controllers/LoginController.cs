17526874d2530bf37a803cb0198b0a4120f2d603
Siranjeevi S <siranjeevicad01@brightskills.com>
2024-04-02 19:05:06 +0530

    edited a style page

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,88 +1,98 @@
-﻿using System.Diagnostics;
+﻿using System;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using I_Can_Rewrite_The_Story.Models;
 using Microsoft.Data.SqlClient;
+using System.Diagnostics;
 
-namespace I_Can_Rewrite_The_Story.Controllers;
-
-
-
-public class LoginController : Controller
+namespace I_Can_Rewrite_The_Story.Controllers
 {
-    private readonly ILogger<LoginController> _logger;
-
-    SqlConnection con=new SqlConnection();
-    SqlCommand com=new SqlCommand();
-    SqlDataReader? dr;
-
-    public LoginController(ILogger<LoginController> logger)
+    public class LoginController : Controller
     {
-        _logger = logger;
-    }
+        private readonly ILogger<LoginController> _logger;
+        private readonly string _connectionString = "Data Source=192.168.1.240\\SQLEXPRESS; database=RMS; User ID=CADBATCH01; Password=CAD@123pass; TrustServerCertificate=True;";
 
-    public IActionResult Index()
-    {
-        return View();
-    }
-
-    public IActionResult AboutUs()
-    {
-        return View();
-    }
-
-    public IActionResult Services()
-    {
-        return View();
-    }
-
-    public IActionResult ContactUs()
-    {
-        return View();
-    }
-
-    public IActionResult Login()
-    {
-        return View();
-    }
-    
-    [HttpGet]
-    public IActionResult Register()
-    {
-        return View();
-    }
-    
-    void ConnectionString(){
-    con.ConnectionString = "Data Source=192.168.1.240\\SQLEXPRESS; database=RMS; User ID=CADBATCH01; Password=CAD@123pass; TrustServerCertificate=True;";
-    }
-    
-    [HttpPost]
-    public IActionResult RegisterDB(RegisterModel rmodel)
-    {
-        ConnectionString();
-        con.Open();
-        com.Connection=con;
-        com.CommandText="insert into Usr_Reg(User_name,Email_Id,Create_Password,Repeat_Password) values(@User_name,@Email_Id,@Create_Password,@Repeat_Password)";
-        com.Parameters.AddWithValue("@User_name",rmodel.User_name);
-        com.Parameters.AddWithValue("@Email_Id",rmodel.Email_Id);
-        com.Parameters.AddWithValue("@Create_Password",rmodel.Create_Password);
-        com.Parameters.AddWithValue("@Repeat_Password",rmodel.Repeat_Password);
-
-        int rowAffected=com.ExecuteNonQuery();
-        if(rowAffected>0){
-            con.Close();
-            return RedirectToAction("Login");
-        }
-        else
+        public LoginController(ILogger<LoginController> logger)
         {
-            con.Close();
-            return View("Error");
+            _logger = logger;
         }
-    }
 
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
+        public IActionResult Services()
+        {
+            return View();
+        }
+
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult Register(RegisterDB rmodel)
+        {
+            if (!ModelState.IsValid)
+            {
+                // If the model is not valid, return the Register view with validation errors
+                return View(rmodel);
+            }
+            
+            try
+            {
+                using (SqlConnection con = new SqlConnection(_connectionString))
+                {
+                    con.Open();
+                    using (SqlCommand com = con.CreateCommand())
+                    {
+                        com.CommandText = "INSERT INTO Usr_Reg (User_name, Email_Id, Create_Password, Repeat_Password) VALUES (@User_name, @Email_Id, @Create_Password, @Repeat_Password)";
+                        com.Parameters.AddWithValue("@User_name", rmodel.User_name);
+                        com.Parameters.AddWithValue("@Email_Id", rmodel.Email_Id);
+                        com.Parameters.AddWithValue("@Create_Password", rmodel.Create_Password);
+                        com.Parameters.AddWithValue("@Repeat_Password", rmodel.Repeat_Password);
+                        int rowsAffected = com.ExecuteNonQuery();
+
+                        if (rowsAffected > 0)
+                        {
+                            return RedirectToAction("Login");
+                        }
+                        else
+                        {
+                            return View("Error");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"An error occurred while registering user: {ex.Message}");
+                return View("Error");
+            }
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
--- Models/RegisterModel.cs
+++ Models/RegisterModel.cs
@@ -2,10 +2,21 @@ using System.ComponentModel.DataAnnotations;
 
 namespace I_Can_Rewrite_The_Story.Models;
 
-public class RegisterModel
+public class RegisterDB
 {
+    [Required(ErrorMessage = "User name is required")]
     public string User_name { get; set; }
+
+    [Required(ErrorMessage = "Email address is required")]
+    [EmailAddress(ErrorMessage = "Invalid email address")]
     public string Email_Id { get; set; }
+
+    [Required(ErrorMessage = "Password is required")]
+    [DataType(DataType.Password)]
     public string Create_Password { get; set; }
+
+    [Required(ErrorMessage = "Please repeat your password")]
+    [Compare("Create_Password", ErrorMessage = "Passwords do not match")]
+    [DataType(DataType.Password)]
     public string Repeat_Password { get; set; }
 }
